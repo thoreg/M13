c8e4240eb3407d01de677c840475530a4f1bd410
Thomas Rega <thoreg@gmail.com>
2016-09-02 20:24:37 +0200

    [ env ] Use a real database
    Keep things simple and use environment variables

--- m13/settings.py
+++ m13/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'm13.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['M13_DATABASE'],
+        'USER': os.environ['M13_DATABASE_USER'],
+        'PASSWORD': os.environ['M13_DATABASE_USER_PASSWD'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
