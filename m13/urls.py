e432f73efa8975648045a727c492eba147118c8e
Thomas Rega <thoreg@gmail.com>
2016-09-08 21:53:27 +0200

    [ sale ] Add simple index page

--- m13/urls.py
+++ m13/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from sale.viewsets import TransactionList
-
+from sale.views import index
 
 urlpatterns = [
     url(r'^api/transactions/', TransactionList.as_view()),
@@ -11,4 +11,6 @@ urlpatterns = [
         include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', index),
 ]
