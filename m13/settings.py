49b124b6b82718146d037bb03793597c7597c03e
Thomas Rega <thoreg@gmail.com>
2016-10-21 18:00:56 +0200

    [ env ] Cosmetic

--- m13/settings.py
+++ m13/settings.py
@@ -43,21 +43,20 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'm13.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+            'sale.context_processors.products'
+        ],
     },
-]
+}]
 
 WSGI_APPLICATION = 'm13.wsgi.application'
 
