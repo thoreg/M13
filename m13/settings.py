101d6d67acf0b02dc77d1672e24db0781e6cb29e
Thomas Rega <thoreg@gmail.com>
2016-10-28 21:36:52 +0200

    [ login ] Customize login template

--- m13/settings.py
+++ m13/settings.py
@@ -194,3 +194,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
 }
+
+LOGIN_URL = '/login/'
--- m13/urls.py
+++ m13/urls.py
@@ -1,24 +1,26 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
-from sale.views import ProductDetailView, index
+
+from sale.views import IndexView, ProductDetailView
 from sale.viewsets import (SalesRankHistoryList, TransactionDayStatsList,
                            TransactionList)
 
 urlpatterns = [
-    url(r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
+    url(r'^api/salesrankhistories/', SalesRankHistoryList.as_view()),
     url(r'^api/stats/', TransactionDayStatsList.as_view()),
     url(r'^api/transactions/', TransactionList.as_view()),
 
-    url(r'^api/salesrankhistories/', SalesRankHistoryList.as_view()),
+    url(r'^maddt/', admin.site.urls),
 
-    url(r'^addi/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}),
+    # url(r'^logout/$', auth_views.logout, name='logout'),
 
-    url(r'^product/(?P<sku>.*)/$',
-        ProductDetailView.as_view(),
-        name='product-detail'),
-    url(r'^$', index),
+
+    url(r'^product/(?P<sku>.*)/$', ProductDetailView.as_view(), name='product-detail'),
+    url(r'^$', IndexView.as_view(), name="index"),
 ]
