449c0a446e106d9e25fbf2821fa77c7953845104
Thomas Rega <thoreg@gmail.com>
2016-11-24 08:46:42 +0100

    [ env ] Split settings into development and production file
    Best practice

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #python manage.py runserver_plus 0.0.0.0:55555
-python manage.py runserver_plus --print-sql 0.0.0.0:55555
+python manage.py runserver_plus --print-sql --settings=m13.settings.development 0.0.0.0:55555
