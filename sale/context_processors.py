c25bb4afb3aa2b6168e278a7f6fbd3b91ad6a001
Thomas Rega <thoreg@gmail.com>
2016-11-26 20:30:27 +0100

    [ nav ] Show product title as requested

--- sale/context_processors.py
+++ sale/context_processors.py
@@ -4,12 +4,20 @@ from sale.models import Product
 
 def products(request):
     """ Used in main navigation """
-    skus_by_category = {}
-    for product in Product.objects.exclude(salesrankhistorybyday__product__isnull=True) \
-                                  .values('category__name', 'sku'):
-        category = product['category__name']
-        if category not in skus_by_category:
-            skus_by_category[category] = []
-        skus_by_category[category].append(product['sku'])
+    product_title_by_category = {}
+    for product in Product.objects \
+            .exclude(salesrankhistorybyday__product__isnull=True) \
+            .values('category__name', 'sku', 'name'):
 
-    return {'skus_with_salesrank_info_by_category': skus_by_category}
+        category = product['category__name']
+
+        if category not in product_title_by_category:
+            product_title_by_category[category] = []
+
+        product_title_by_category[category].append(
+            (product['sku'], product['name'][:55]))
+
+    for category, products in product_title_by_category.items():
+        product_title_by_category[category] = sorted(products)
+
+    return {'skus_with_salesrank_info_by_category': product_title_by_category}
