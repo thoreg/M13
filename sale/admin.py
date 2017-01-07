ac724e4c4b5a627e23f97d29779c033231ca4778
Thomas Rega <thoreg@gmail.com>
2017-01-07 12:53:00 +0100

    [ sale ] pep8

--- sale/admin.py
+++ sale/admin.py
@@ -6,23 +6,21 @@ from .models import Category, Product, SubCategory, Transaction
 class TransactionAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Transaction, TransactionAdmin)
-
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category')
     list_filter = ('category',)
 
-admin.site.register(Product, ProductAdmin)
-
 
 class CategoryAdmin(admin.ModelAdmin):
     ordering = ('name',)
-
-admin.site.register(Category, CategoryAdmin)
 
 
 class SubCategoryAdmin(admin.ModelAdmin):
     pass
 
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Transaction, TransactionAdmin)
